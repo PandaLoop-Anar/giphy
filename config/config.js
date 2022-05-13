e953649d9ecf14ece2936b906953ce677fe9073d
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-13 18:11:10 +0400

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,10 @@
+
+// const queryURL = "https://api.giphy.com/v1/gifs/search?";
+// const queryURL = "https://api.giphy.com/v1/gifs/trending?";
+let query;
+const params = {
+  q: query,
+  limit: 10,
+  api_key: "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
+  fmt: "json"
+};
