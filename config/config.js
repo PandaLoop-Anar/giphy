69055bd146a6798294c4e07c7b422514a8265928
Anar Mamedovi <84316960+PandaLoop-Anar@users.noreply.github.com>
2022-05-15 00:32:15 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 
 // const queryURL = "https://api.giphy.com/v1/gifs/search?";
 // const queryURL = "https://api.giphy.com/v1/gifs/trending?";
+export const queryURL = "https://api.giphy.com/v1/gifs/";
 let query;
-const params = {
-  q: query,
+export const params = {
+  // q: query,
   limit: 10,
   api_key: "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB",
   fmt: "json"
